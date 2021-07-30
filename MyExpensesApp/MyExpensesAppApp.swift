a9be591aca1f67babf3ecba92dc33ab41f1d2999
Andrii Malnov <andrey.malnov@gmail.com>
2021-07-30 16:49:44 +0300

    Initial Commit

---
+++ MyExpensesApp/MyExpensesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyExpensesAppApp.swift
+//  MyExpensesApp
+//
+//  Created by Mac on 30.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyExpensesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
